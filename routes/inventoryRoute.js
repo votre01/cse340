5c1f42d03942a9308dfcc63eba61beaf87f9fcad
Trevor <ngw21001@byui.edu>
2024-01-24 10:24:59 +0200

    week03

---
+++ routes/inventoryRoute.js
@@ -0,0 +1,9 @@
+// Needed resources
+const express = require("express")
+const router = new express.Router()
+const invController = require("../controllers/invController")
+
+// Route to build inventory by classification
+router.get("/type/:classificationId", invController.buildByClassificationId);
+
+module.exports = router;
